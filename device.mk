0ed5b8d46024554f470ea32c8e5a02df1a2f991c
bengris32 <bengris32@protonmail.ch>
2022-12-29 16:58:41 +0000

    begonia: Add hardware/mediatek to Soong namespaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I0c679ef23ab747a9c0ce2d8db31d344cfb5c1418
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>
    Signed-off-by: Neebe3289 <neebexd@gmail.com>

--- device.mk
+++ device.mk
@@ -326,7 +326,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Tethering
 PRODUCT_PACKAGES += \
